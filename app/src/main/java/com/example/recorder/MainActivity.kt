8c5d4e330017a087dda7a1dcd9c516ada27a0944
yuliaivanenko <y-ivanenko@alfabank.ru>
2021-05-22 14:20:03 +0300

    add fragments and bottom navigation

--- app/src/main/java/com/example/recorder/MainActivity.kt
+++ app/src/main/java/com/example/recorder/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.recorder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.recorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment_container)
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
     }
 }
